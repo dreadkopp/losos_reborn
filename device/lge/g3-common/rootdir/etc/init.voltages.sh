08a6b6b8fcd12c9eff91b713141e35e515ba8710
Arne Ziegert <arne@Arnes-MBP.fritz.box>
2018-01-21 19:40:13 +0100

    switched to qCOM policies from 1+1 bacon + tidy up scripts a bit

--- device/lge/g3-common/rootdir/etc/init.voltages.sh
+++ device/lge/g3-common/rootdir/etc/init.voltages.sh
@@ -13,4 +13,22 @@ for i in "${base_voltages[@]}"; do
     voltages_corrected="$voltages_corrected $i"
 done
 
+#if standard break here
 echo $voltages_corrected > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
+
+
+# apply OC settings here
+
+MAX_FREQ=2726400
+
+#if powersave set MAX_FREQ to 1,5Ghz
+
+echo 0 > /sys/module/msm_thermal/core_control/enabled
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
+
+echo $MAX_FREQ > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo $MAX_FREQ > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo $MAX_FREQ > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo $MAX_FREQ > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
