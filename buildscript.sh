d1eaab1c4032438ac13143329efadb0c7f1b875a
dreadkopp <arne.ziegert@gmx.de>
2018-01-21 20:39:20 +0100

    fixed typo in manifest and updated buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #script to build losos automagically for all devices
 #run from root dir of sources
-
+cp losos/.repo/local_manifests/roomservice.xml .repo/local_manifests/roomservice.xml
+./repo sync
 cp losos/* ./*
 rm -r losos/packages/apps/*/Android.mk
 rm -r out/target/product/*/*.zip
