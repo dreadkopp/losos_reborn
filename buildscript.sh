7991090bec75b6a0d32dd9b661703bb71fa4ed1f
dreadkopp <arne.ziegert@gmx.de>
2018-01-21 20:32:31 +0100

    simple script to automate things a bit

---
+++ buildscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#script to build losos automagically for all devices
+#run from root dir of sources
+
+cp losos/* ./*
+rm -r losos/packages/apps/*/Android.mk
+rm -r out/target/product/*/*.zip
+brunch d855
+brunch d850
+brunch d851
+brunch d852
+brunch vs985
+brunch ls990
+brunch f400
